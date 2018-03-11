585a50331f9e78e07d17cbf440ba9f06ecf8056b
Alex Roman <eirexe123@gmail.com>
2018-03-11 23:17:38 +0100

    Add sanity checks to key remapping and allow new events in actions to be defined by the user

--- game/System/Scripts/Scenes/Menus/OptionsMenu.gd
+++ game/System/Scripts/Scenes/Menus/OptionsMenu.gd
@@ -6,7 +6,10 @@ onready var msaa_option_button = get_node("TabContainer/Video/VBoxContainer/HBox
 onready var fxaa_option_checkbox = get_node("TabContainer/Video/VBoxContainer/HBoxContainer/LeftCol/FXAA/FXAAOption")
 onready var show_fps_option_checkbox = get_node("TabContainer/Video/VBoxContainer/HBoxContainer/LeftCol/ShowFPS/ShowFPSOption")
 
-onready var input_map_tree =  get_node("TabContainer/Controls/InputMapTree")
+onready var input_map_tree =  get_node("TabContainer/Controls/VSplitContainer/InputMapTree")
+
+onready var user_alert_dialog = get_node("UserAlertDialog")
+
 const ACTION_NAME_REMAPS_FILE = "res://System/Data/action_name_remaps.json"
 const MOUSE_BUTTON_REMAPS_FILE = "res://System/Data/mouse_button_remaps.json"
 var action_name_remaps
@@ -38,22 +41,38 @@ func _input(event):
 		if event is InputEventKey:
 			if event.scancode == KEY_ESCAPE:
 				cancel_input_remap()
-				get_tree().set_input_as_handled()
 				return
+				
+		# Check if input is already in use...
+				
+		for action in InputMap.get_actions():
+			if InputMap.action_has_event(action, event):
+				
+				get_tree().set_input_as_handled()
+				user_alert_dialog.dialog_text = "Key %s is already in use by %s" % [event2str(event), beautify_action(action)]
+				user_alert_dialog.popup()
+				
+				cancel_input_remap()
+				
+				
+				return
+				
+				
 		var item = input_map_tree.get_selected()
 		var meta = item.get_metadata(0)
 		if meta:
-			cancel_input_remap()
+			get_tree().set_input_as_handled()
 			meta["event"] = event
 			item.set_text(0, event2str(event))
 			item.set_metadata(0, meta)
-
+			cancel_input_remap()
 func init_video_settings():
 	# MSAA settings
 	msaa_option_button.select(EROSettings.msaa)
 	show_fps_option_checkbox.pressed = EROSettings.show_fps
 	fxaa_option_checkbox.pressed = EROSettings.fxaa
 func init_input_settings():
+	input_map_tree.clear()
 	var root = input_map_tree.create_item()
 	input_map_tree.set_hide_root(true)
 	for action in InputMap.get_actions():
@@ -69,13 +88,15 @@ func init_input_settings():
 func confirmed():
 	EROSettings.msaa = msaa_option_button.selected
 	EROSettings.show_fps = show_fps_option_checkbox.pressed
-	EROSettings.save_settings()
 	save_input()
+	EROSettings.save_settings()
+
 
 func about_to_show():
 	cancel_input_remap()
-	init_video_settings() 
+	init_video_settings()
 	init_input_settings()
+	print("reinit")
 
 
 func input_item_double_clicked():
@@ -87,7 +108,10 @@ func cancel_input_remap():
 	if item:
 		if item.get_metadata(0).has("event"):
 			var event = item.get_metadata(0)["event"]
-			item.set_text(0, event2str(event))
+			if event:
+				item.set_text(0, event2str(event))
+			else:
+				item.free()
 	
 func save_input():
 	var tree_root = input_map_tree.get_root()
@@ -112,3 +136,31 @@ func event2str(event):
 		return mouse_button_remaps[str(event.button_index)]
 	else:
 		return event.as_text()
+		
+func add_action():
+	var item = input_map_tree.get_selected()
+	if item:
+		if is_item_action(item):
+			item.deselect(0)
+			var new_item = input_map_tree.create_item(item)
+			new_item.select(0)
+			new_item.set_metadata(0, {"event": null})
+			input_item_double_clicked()
+		
+func beautify_action(action):
+	if action_name_remaps.has(action):
+		return action_name_remaps[action]
+	else:
+		return action
+		
+func is_item_action(item):
+	if item.get_metadata(0):
+		return item.get_metadata(0).has("action")
+	else:
+		return false
+		
+func is_item_event(item):
+	if item.get_metadata(0):
+		return item.get_metadata(0).has("event")
+	else:
+		return false
